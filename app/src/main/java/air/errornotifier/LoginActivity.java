eb6e6f5668f9762d98a7fbe1f50765fa96cb5d70
Oliver Grbavac <ogrbavac@foi.hr>
2017-11-20 21:02:05 +0100

    Napravljena prijava, ali se app zamrze prilikom klika na gumb Login.

--- app/src/main/java/air/errornotifier/LoginActivity.java
+++ app/src/main/java/air/errornotifier/LoginActivity.java
@@ -1,28 +1,26 @@
 package air.errornotifier;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import org.json.JSONObject;
-
-import java.util.concurrent.ExecutionException;
-
-import air.core.AddNewUser;
-import air.core.Bean.Users;
 import air.core.LogInCall;
-import air.database.helper.Constants;
-import air.webservices.LogIn;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private static String TAG = "MAIN ACTIVITY";
+    EditText UsernameEt, PasswrodEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        UsernameEt = (EditText)findViewById(R.id.txtUsername);
+        PasswrodEt = (EditText)findViewById(R.id.txtPassword);
 
     }
+
 
     /*private void kreiraj_novog() throws ExecutionException, InterruptedException {
         Evo ovdi je napravljeno kak se zove dok ADMIN hoce dodati novoga korisnika...
@@ -40,18 +38,22 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "Potkrala se greska");
         }
     }*/
-
-    private void prijava(){
+    
+    public void prijava(View view){
         //dok klikne na button prijava onda nek se pozove ovo samo umjesto jbond upisite ovo
         //sto je getano u textinputima...
-        LogInCall logInCall= new LogInCall("jbond","jbond");
+        String username = UsernameEt.getText().toString();
+        String password = PasswrodEt.getText().toString();
+        LogInCall logInCall= new LogInCall(username, password);
         if (logInCall.getUser().getFirstName() !=null) {
+            Toast.makeText(LoginActivity.this, "Uspješna prijava!!!", Toast.LENGTH_SHORT).show();
             Log.i("MAIN", "Nije prazno" );
             //provjeri dal je admin Constant.TYPE = 1 (to bude za admin)
             //prebaci se na aktivnost logiran admin
             //else provjeri dal je korisnik
             //prebaci se na mod korisnik
         } else {
+            Toast.makeText(LoginActivity.this, "Pogrešan username ili lozinka", Toast.LENGTH_SHORT).show();
             Log.i("MAIN", "NETOCNA PRIJAVA");
             //netocna prijava
         }
