c3850d2dac990e387b51e87e70db48b7cabafd49
tomica93 <tomi.hranj@gmail.com>
2017-11-28 22:33:07 +0100

    Dodani su setOnClickListern-i za FloatingActionButton-e.

--- app/src/main/java/air/errornotifier/MainActivity.java
+++ app/src/main/java/air/errornotifier/MainActivity.java
@@ -1,12 +1,17 @@
 package air.errornotifier;
 
-import android.support.design.widget.FloatingActionButton;
+
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +19,15 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private TabLayout mTabLayout;
-    private FloatingActionButton mFabPlus, mFabAddUser;
-    boolean isOpen = false;
+ //   private FloatingActionMenu floatingActionMenu;
+    private FloatingActionButton fabUser,fabGroup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Dodavanje ActionBar-a
         mTolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mTolbar);
         getSupportActionBar().setTitle("ErrorNotifier");
@@ -29,12 +36,39 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.tabPager);
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
+        //ViewPager
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
+        //TabLayout
         mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
         mTabLayout.setupWithViewPager(mViewPager);
 
         //FloatingAction
+  //      floatingActionMenu = (FloatingActionMenu) findViewById(R.id.menuFab);
+        fabUser = (FloatingActionButton) findViewById(R.id.menuFabAddUser);
+        fabGroup = (FloatingActionButton) findViewById(R.id.menuFabAddGroup);
+
+//        floatingActionMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            Toast.makeText(MainActivity.this,"klik", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        fabUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"korisnik",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        fabGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"grupa",Toast.LENGTH_LONG).show();
+            }
+        });
+
 
 
 
