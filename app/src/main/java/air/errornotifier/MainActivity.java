6f4326ed3343b0d62ae741bd77553a50ae532c0a
tomica93 <tomi.hranj@gmail.com>
2017-11-28 17:50:37 +0100

    Uklonjen ActionBar na LoginActivity-u i ručno dodan na MainActivitiy.

--- app/src/main/java/air/errornotifier/MainActivity.java
+++ app/src/main/java/air/errornotifier/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar mTolbar;
     private ViewPager mViewPager;
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private TabLayout mTabLayout;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mTolbar);
+        getSupportActionBar().setTitle("ErrorNotifier");
 
         //Tabs
         mViewPager = (ViewPager) findViewById(R.id.tabPager);
