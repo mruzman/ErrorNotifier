69bbb33d67b1f6e345acace6cc3f4e070b4cc775
mileticm <mmiletic2@foi.hr>
2018-01-22 00:44:47 +0100

    EmailAdapter

--- app/src/main/java/air/errornotifier/EmailActivity.java
+++ app/src/main/java/air/errornotifier/EmailActivity.java
@@ -5,15 +5,29 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import air.database.Bean.App;
+import air.database.Bean.Email;
 
 public class EmailActivity extends AppCompatActivity {
     private Toolbar mTolbar;
+    private List<Email> emailList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private EmailViewAdapter emailViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,30 @@ public class EmailActivity extends AppCompatActivity {
         mTolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mTolbar);
         getSupportActionBar().setTitle("Emails");
+
+        //
+        recyclerView = findViewById(R.id.recycler2);
+        emailViewAdapter = new EmailViewAdapter(emailList);
+        /*try {
+            //dohvati emailove
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();*/
+
+        emailViewAdapter = new EmailViewAdapter(emailList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(emailViewAdapter);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
